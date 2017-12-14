b8e9ed60c7cdf2970c154a1d07db0bea2a148359
ffzs <dz759043338@163.com>
2017-12-14 18:00:12 +0800

    第二次修改

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ CITY = "beijing-530" #beijing-530 , tianjing-531 , dalian-600 , qingdao-703
 KEYWORD = "python"
 
 MONGO_URL='localhost'
-MONGO_DB="ZLZP-"+CITY+"-"+KEYWORD
-MONGO_TABLE="ZLZP-"+CITY+"-"+KEYWORD
+MONGO_DB="ZhiLian-"+CITY+"-"+KEYWORD
+MONGO_TABLE="ZhiLian-"+CITY+"-"+KEYWORD
 
 
 
