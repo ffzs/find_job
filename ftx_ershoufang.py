c533db069c1d055edbe2dba679f0e8263a32c379
ffzs <dz759043338@163.com>
2017-12-16 09:22:11 +0800

    5

--- ftx_ershoufang.py
+++ ftx_ershoufang.py
@@ -2,23 +2,50 @@
 import requests
 import random
 import json
-
-
-headers = {
-        'Referer': 'http://lbs.amap.com/console/show/picker',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-        }
+from config import *
+from multiprocessing import Pool
 
 def get_ip_list(file):
     file = open(file)
     ip_list =[]
     for line in file:
-        ip_list.append(line.strip())
+        try:
+            ip_list.append(json.loads(line.strip()))
+        except:
+            pass
     return ip_list
 
+def make_sure(ip_dict):
+    headers = {
+        'Referer': 'https://m.fang.com/city/hotcity.jsp?city=bj',
+        'User-Agent': random.choice(USER_AGENTS)
+    }
+    url = 'https://m.fang.com/zf/bj/'
+    try:
+        response = requests.get(url, headers=headers,proxies=ip_dict)
+        print(url,ip_dict,response)
+        return True
+    except Exception as e:
+        print(ip_dict,e)
+        return False
+
+def main(num):
+    if make_sure(IP_LIST[num]):
+        with open("ip_ftx.txt", "a", encoding='utf-8') as f:
+            jsoninfo = json.dumps(IP_LIST[num])
+            print(jsoninfo)
+            f.write(jsoninfo + "\n")
+            f.close()
+
+
 if __name__ == '__main__':
-    ip_list = get_ip_list("ip_.txt")
-    url ="https://m.fang.com/zf/?purpose=%D7%A1%D5%AC&jhtype=zf&city=%B1%B1%BE%A9&renttype=cz&c=zf&a=ajaxGetList&city=bj&r=0.8237764370327307&page=4"
-    response = requests.get(url=url,headers= headers,proxies = json.loads(random.choice(ip_list))).content.decode("utf-8")
-    print(response)
+    # MY_ip_list = get_ip_list("ip_.txt")
+    # print(MY_ip_list)
+    l = len(IP_LIST)
+    groups = [x for x in range(0,l)]
+    pool = Pool()
+    pool.map(main, groups)
+
+
+
 
